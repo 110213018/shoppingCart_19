7f6067f0f755d5517f3c7f926dd173ea7f5484f3
minmin <minmin0705.chen@gmail.com>
2024-01-09 21:07:52 +0800

    initial commit

---
+++ frontend/src/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <LoginForm v-if="store.token === null"/>
+  <Buyer v-if="store.token !== null && store.user.role === 1"/>
+  <Sale v-if="store.token !== null && store.user.role === 2"/>
+  <Logistics v-if="store.token !== null && store.user.role === 3"/>
+</template>
+
+<script setup>
+import { useAuthStore } from '@/store/app';
+
+const store = useAuthStore();
+</script>
